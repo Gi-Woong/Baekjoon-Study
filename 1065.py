1ab8207e8023d4bed5f9391c5282713919fecd76
dipito <rldndco@mju.ac.kr>
2021-11-10 09:30:25 +0900

    Create 1065.py

---
+++ 1065.py
@@ -0,0 +1,26 @@
+def ishansu(n):
+    num = n
+    L = []
+    while n != 0:
+        L.append(n % 10)
+        n //= 10
+    L.reverse()
+    #각 자리 수가 1개 이하인 경우
+    #어쨋든 등차수열(?)이므로 True 반환
+    if len(L) <= 1:
+        return True
+    #각 자리 수가 2개 이상인 경우
+    d = L[1] - L[0] #공차
+    for i in range(len(L)):
+        Ai = L[0] + i*d #일반항
+        #일반항의 계산결과가 실제와 다르면 False 반환
+        if Ai != L[i]:
+            return False
+    return True
+
+num = int(input())
+ans = 0
+for i in range(1,num+1):
+    if ishansu(i):
+        ans += 1
+print(ans)
