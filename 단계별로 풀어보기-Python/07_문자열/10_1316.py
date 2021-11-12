403d9be95a5a84aa7cb46b33314ac4f29e3720b2
dipito <rldndco@mju.ac.kr>
2021-11-12 23:58:54 +0900

    solving

--- 단계별로 풀어보기-Python/07_문자열/10_1316.py
+++ 단계별로 풀어보기-Python/07_문자열/10_1316.py
@@ -1,2 +1,26 @@
 #제목: 그룹 단어 체커
 #정보: 조건에 맞는 문자열을 찾는 문제
+
+def isgroup(S):
+    L = [S[0]]
+    prev = S[0]
+    for s in S:
+        if prev != s:
+            if not s in L:
+                L.append(s)
+                prev = s
+            else:
+                return False
+    return True
+
+N = int(input())
+cnt = 0
+for i in range(N):
+    S = input()
+    if isgroup(S):
+        cnt += 1
+print(cnt)
+
+
+
+
