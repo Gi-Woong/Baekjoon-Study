eaff651149a758ff4750c82f38aaf5e84d6b5821
dipito <77622897+Gi-Woong@users.noreply.github.com>
2021-11-28 17:14:31 +0900

    commit

--- 단계별로 풀어보기-Python/08_기본 수학 1/04_2869.py
+++ 단계별로 풀어보기-Python/08_기본 수학 1/04_2869.py
@@ -1,2 +1,11 @@
 #제목: 달팽이는 올라가고 싶다
 #정보: 달팽이의 움직임을 계산하는 문제
+
+#A: 낮에 달팽이가 올라갈 수 있는 길이
+#B: 밤에 달팽이가 미끄러지는 길이
+#V: 나무막대 높이
+
+import math
+A, B, V = map(int, input().split())
+day = math.ceil((V - A)/(A - B)) + 1
+print(day)
--- 단계별로 풀어보기-Python/08_기본 수학 1/05_10250.py
+++ 단계별로 풀어보기-Python/08_기본 수학 1/05_10250.py
@@ -1,2 +1,17 @@
 #제목: ACM 호텔
 #정보: 호텔 방 번호의 규칙을 찾아 출력하는 문제
+
+#H: 호텔 층 수
+#W: 호텔 층당 호 수
+#N: 손님의 번째
+
+repeat = int(input())
+for i in range(repeat):
+    H, W, N = map(int, input().split())
+    if N%H == 0: 
+        floor = H
+        ho = N//H
+    else:
+        floor = N%H
+        ho = N//H+1
+    print(f"{floor}{str(ho).zfill(2)}")
